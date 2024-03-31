adcb7037ae088e39f50144ae4c0e2e341816a6a7
BobadillaLeonRodrigo <al222110815@gmail.com>
2024-03-31 15:08:29 -0600

    Consulta y logica de citas

--- app/Http/Controllers/ControllerCitas.php
+++ app/Http/Controllers/ControllerCitas.php
@@ -4,16 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Models\Citas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ControllerCitas extends Controller
 {
     public function citas()
     {
-        // $TipoRol =  \DB::select('SELECT usuarios.ID_Usuario,usuarios.Nombre_Usuario,usuarios.Apellido_Paterno,usuarios.Apellido_Materno,usuarios.Email,usuarios.ID_Role, tipo_roles.ID_Role, tipo_roles.Nombre_Role
-        //                             FROM tipo_roles, usuarios
-        //                                 WHERE usuarios.ID_Role = tipo_roles.ID_Role
-        // ');
-        $Citas = Citas::all();
+        $Citas = \DB::select('SELECT citas.`ID_Cita`,citas.`Fecha`,citas.`Hora`,citas.`Comentario`,citas.`ID_Usuario`,citas.`ID_TipoCitas`,usuarios.`ID_Usuario`,usuarios.`Nombre_Usuario`,tipo_citas.`ID_TipoCitas`,tipo_citas.`Descripcion`
+                                FROM citas,usuarios,tipo_citas
+                                    WHERE citas.`ID_Usuario` = usuarios.`ID_Usuario` AND citas.`ID_TipoCitas` = tipo_citas.`ID_TipoCitas`');
         return view('admin.citas.index', compact('Citas'));
+    }
+
+    public function agregarCi(Request $request)
+    {
+        //dd($request->all());
+        Citas::create(array(
+            'ID_Citas' => $request->input('ID_Citas'),
+            'Fecha' => $request->input('Fecha'),
+            'Hora' => $request->input('Hora'),
+            'Comentario' => $request->input('Comentario'),
+            'ID_Usuario' => $request->input('ID_Usuario'),
+            'ID_TipoCitas' => $request->input('ID_TipoCitas')
+        ));
+        return redirect()->route("citas");
+    }
+    public function eliminarCi(Citas $id)
+    {
+        // Eliminar el registro
+        $id->delete();
+
+        // Reorganizar las ID
+        DB::statement('SET @count = 0');
+        DB::statement('UPDATE Citas SET ID_Cita = @count:= @count + 1');
+        DB::statement('ALTER TABLE Citas AUTO_INCREMENT = 1');
+
+        // Redireccionar o retornar una respuesta según sea necesario
+        return redirect()->route('citas');
+    }
+
+    public function editarCi(Citas $id)
+    {
+        $citas = Citas::all();
+
+        return view("admin.citas.edit")
+            ->with(['citas' => $id])
+            ->with(['tipos' => $citas]);
+    }
+
+    public function salvarCi(Citas $id, Request $request)
+    {
+        //$id->update($request->only('clave','nombre','app','fn','gen','foto','email','pass','nivel','activo'))
+        //dd($request->all());
+        $query = Citas::find($id->ID_Citas);
+        $query->Fecha = $request->Fecha;
+        $query->Hora = trim($request->Hora);
+        $query->Comentario = trim($request->Comentario);
+        $query->ID_Usuario = $request->ID_Usuario;
+        $query->ID_TipoCitas = $request->Contraseña;
+        $query->save();
+        return redirect()->route("citas", ['id' => $id->ID_Citas]);
     }
 }
